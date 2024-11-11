4d54f971866694eaaa138ee3dcf9d76b84fa5322
Ronald-8-ball <ronaldokuk462@gmail.com>
2024-11-11 20:56:01 +0100

    Fun shapes

--- SNE.py
+++ SNE.py
@@ -2,17 +2,12 @@ import turtle
 turtle.Screen().bgcolor("pink")
 # Start a work Screen
 board = turtle.Screen()
- 
 # Define a Turtle Instance
 board = turtle.Turtle()
- 
 # executing loop 6 times for 6 sides
 for i in range(6):
- 
     # Move forward by 90 units
     board.forward(90)
- 
     # Turn left the turtle by 300 degrees
     board.left(300)
-
 turtle.done()
